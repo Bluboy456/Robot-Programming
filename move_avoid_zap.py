e7fdbb5069add8daf1d8f2b1b818790ec2406eff
Bluboy456 <stuart.jessup@hotmail.co.uk>
2020-12-16 17:51:18 +0000

    Move_base working and traversing code added but not tested Mapping phase terminates cleanly now

--- move_avoid_zap.py
+++ move_avoid_zap.py
@@ -13,6 +13,8 @@ import cv2
 import numpy as np
 from uol_cmp9767m_tutorial.srv import *
 from std_srvs.srv import Empty
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 class mapping():
 
@@ -33,7 +35,7 @@ class mapping():
         self.mapping_start_time = time.time()
         self.mapping_complete = False   #flag that can be interpreted to find out whether mapping is complete
         self.mapping_duration = 2      #duration of mapping stage in seconds
-        self.occupancy_grid = []
+        self.mapping_complete = False
         self.rate = rospy.Rate(3) 
 
 
@@ -49,11 +51,8 @@ class mapping():
                                                 #obstacles, used for initial mapping
     
         # check whether mapping time is over
-        if (time.time() - self.mapping_start_time) > self.mapping_duration: 
-            self.occupancy_grid = rospy.wait_for_message('/map',OccupancyGrid )
-            #print self.occupancy_grid #DEBUG
-            #print'time up' #DEBUG
-            rospy.signal_shutdown('mapping over')
+        if (time.time() - self.mapping_start_time) > self.mapping_duration:
+            self.mapping_shutdown()
             return
 
         self.ranges = msg.ranges
@@ -84,6 +83,9 @@ class mapping():
             self.motion.angular.z = angular_vel
             self.cmd_vel_pub.publish(self.motion)
 
+    def mapping_shutdown(self):
+        self.mapping_complete = True
+        self.laser_sub.unregister()  
 
     def rotate(self, direction):
         self.motion.linear.x = 0.0
@@ -132,111 +134,101 @@ class mapping():
         return (Point(*trans))   
 
     
-    
-
-'''   DEBUG Commented out while refactoring mapping class
-class move_find_green:
-
-
-    # move foward but turn  if within 1m of an obstacle
-
+class weeding:   
 
     def __init__(self):
-        #mapping parameters
-        self.direction = 'left'
-        self.obstacle_x = 10.0      
-        self.obstacle_y = 10.0   
-        
-        #state flags
-        self.movement_paused = False
-        self.mapping = True
-
-        self.laser_sub = rospy.Subscriber('/thorvald_001/scan', LaserScan, self.laser_callback_mapping)   #subscribe to laser to get distance messages to get distances
+        self.occupancy_grid = rospy.wait_for_message('/map',OccupancyGrid )
+        self.meta_data = rospy.wait_for_message('/map_metadata',MapMetaData ) 
+        self.move_base_client = actionlib.SimpleActionClient('/move_base/',MoveBaseAction)
+        resolution = self.meta_data.resolution  # metres per cell of occupancy grid
+        self.grid_width = self.meta_data.width * resolution  #convert width from cell count to metres
+        self.grid_height = self.meta_data.height * resolution
+        self.grid_origin_x = self.meta_data.origin.position.x
+        self.grid_origin_y = self.meta_data.origin.position.y
+        '''
         self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",Image,self.camera_callback)
-        self.cmd_vel_pub = rospy.Publisher('/thorvald_001/twist_mux/cmd_vel', Twist, queue_size=1)  #publisher for speed and turn to robot
-        self.motion = Twist()
         self.bridge_object = CvBridge()
         self.tf_listener = tf.TransformListener()
         self.calculate_camera_sprayer_transform()
-       
-        self.rate = rospy.Rate(3)
+        '''
+    
+    # moves robot acroos entire map
+    # assumes crop planted in y direction so traverses are in y direction
+    def scan_map(self):
+        self.traverse_spacing = 1     #spacing in metres between traverses
+        self.margin = 1               #distance from boundary of the turning points at end of a traverse (metres)
+
+    # move to cell (0,0), ie the'bottom left' corner, allowing for margins
+        self.next_x = self.grid_origin_x + self.margin
+        self.next_y = self.grid_origin_y + self.margin
+        self.move_to_goal (self.next_x, self.next_y)
+        self.current_x = self.next_x
+        self.current_y = self.next_y
+
+
+        while True:                   
+        # make a traverse   
+            self.next_x = self.current_x 
+            self.next_y = self.current_y + (self.grid_height - self.margin)
+            self.move_to_goal (self.next_x, self.next_y)
+            self.current_x = self.next_x
+            self.current_y = self.next_y
+
+        #move across    
+            self.next_x = self.current_x + self.traverse_spacing
+            if self.next_x > (self.grid_width - self.margin):
+                break                                       #gone all the way across
+            self.next_y
+            
+            self.current_y
+            self.move_to_goal (self.next_x, self.next_y
+            )
+            self.current_x = self.next_x
+            self.current_y = self.next_y
+
+        # traverse  back 
+            self.next_x = self.current_x 
+            self.next_y
+            
+            self.current_y  - (self.height - self.margin)
+            self.move_to_goal (self.next_x, self.next_y
+            )
+            self.current_x = self.next_x
+            self.current_y = self.next_y
+
+        #move across to complete one cycle up and down
+            self.next_x = self.current_x + self.traverse_spacing
+            if self.next_x > (self.grid_width - self.margin):
+                break                                       #gone all the way across
+            self.next_y
+            
+            self.current_y
+            self.move_to_goal (self.next_x, self.next_y
+            )
+            self.current_x = self.next_x
+            self.current_y = self.next_y
 
 
 
-        rospy.spin()
-
-    def laser_callback_mapping (self, msg):     # called when a laser message arrives,moves randomly and avoids
-                                        #obstacles, used for initial mapping
-        if not(self.mapping):
-            return                      # only run this callback when in mapping state. 
-        if self.movement_paused:
-            return                      #don't move if pause in progress
-        rospy.spin
-        self.ranges = msg.ranges
-        self.angle_increment = msg.angle_increment
-        self.clearance_right, self.angle_right = self.right_sector_clearance()
-        self.clearance_left, self.angle_left = self.left_sector_clearance()         
-        
-        if (self.clearance_right < 1.0 or self.clearance_left <1.0): 
-            self.obstacle_present = True   #Flag used by move_to_spray method to abort if obstacle in the way
+    # The following function is adapted from move_base action_client example at 
+    # hotblackrobotics.github.io/en/blog/2018/01/29/action-client-py/
+    def move_to_goal (self, x_goal, y_goal):
+        self.move_base_client.wait_for_server()
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x_goal
+        goal.target_pose.pose.position.y = y_goal
+        goal.target_pose.pose.orientation.w = 1.0
+        self.move_base_client.send_goal(goal)
+        wait = self.move_base_client.wait_for_result()
+        if not wait:
+            rospy.logerr("Action server not available!")
+            rospy.signal_shutdown("Action server not available!")
         else:
-            self.obstacle_present = False
+            return self.move_base_client.get_result()
 
-        #does it need to turn left?
-        if self.clearance_right < 1.0:  
-            self.rotate('left')
-        
-        #does it need to turn right?
-        elif self.clearance_left <1.0:
-            self.rotate('right')
-
-        else:  #free to move forward
-            self.motion.linear.x = self.linear_speed 
-            # introduce a turn sometimes to give a random element to motion
-            # dont use random function as that will average to zero over callbacks
-            time_in_secs_mod_ten = int(round(time.time())) % 10
-            angular_vel = 0.0
-            if (time_in_secs_mod_ten < 5):
-                angular_vel = (time_in_secs_mod_ten-2)/10.0
-            print 'random angular velocity: ' + str (angular_vel)  #DEBUG
-
-
-            self.motion.angular.z = angular_vel
-            self.cmd_vel_pub.publish(self.motion)
-
-
-    def rotate(self, direction):
-        self.motion.linear.x = 0.0
-        if self.direction == 'right':
-            self.motion.angular.z = -1 * self.angular_speed   
-        else:
-            self.direction = 'left'
-            self.motion.angular.z = self.angular_speed  
-        self.cmd_vel_pub.publish(self.motion)    
-
-
-    # returns minium distance in right front 90deg sector and angle to that closest obstacle
-    def right_sector_clearance(self):
-        self.min_distance = 10.0
-        self.angle_to_closest = 0.0
-        for index in range(0,len(self.ranges)//2-1):
-            if self.min_distance > self.ranges[index]:  
-                self.min_distance = self.ranges[index]    #found a new minium distance
-                self.angle_to_closest = -1*((len(self.ranges)//2-1) - index ) * self.angle_increment  # convert to radians  
-                                                # angle of zero must be straight ahead 
-                                                # clockwise angle (to right) so negative in polar co-ords
-        return self.min_distance, self.angle_to_closest
-
-# returns minium distance in left front 90deg sector and angle to that closest obstacle
-    def left_sector_clearance(self):
-        self.min_distance = 10.0  
-        self.angle_to_closest = 0.0
-        for index in range(len(self.ranges)//2,len(self.ranges)-1):
-            if self.min_distance > self.ranges[index]:  
-                self.min_distance = self.ranges[index]  #found a new minium distance
-                self.angle_to_closest = index * self.angle_increment  # convert to radians
-                                                # anticlockwise angle (to left) so positive in polar co-ords
-        return self.min_distance, self.angle_to_closest
+'''
 
     def calculate_camera_sprayer_transform(self):
     # calculate offset from camera to sprayer so that sprayer can be moved over target
@@ -286,6 +278,7 @@ class move_find_green:
         
         # spray if blob (weed) found TODO move robot to spray in correct place
         if keypoints:
+            # TODO Cancel current movebase and push goal to stack
             self.move_over_and_spray()
 
         # Show blobs
@@ -380,22 +373,31 @@ class move_find_green:
 '''
 # Main Code
 if __name__ == '__main__':
+    # CREATE MAP
     rospy.init_node('mapping', anonymous=True)
     try:
         m = mapping()
+        '''
         m.map()
-        print m.occupancy_grid
-        rospy.spin()
+        #wait while mapping phase takes place
+        while not(m.mapping_complete):
+            time.sleep(1)
+        '''
+        # TRAVERSE FIELD, ID WEEDS AND SPRAY
+        #rospy.init_node('weeding', anonymous=True)
+        w = weeding()        
+        move_result = w.move_to_goal(100,100)
+        if move_result:
+            rospy.loginfo("Goal reached")
+
+
+
     except rospy.ROSInterruptException:
         rospy.logwarn("interrupted")
         pass
-    #while m.mapping_complete == False:
-        # wait for mapping phase to complete
-    #print 'from main: mapping in progress'
-    print 'from main: mapping over'
 
 
-   
+
 
 
  
