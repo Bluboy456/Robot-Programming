91625982c0773d02b01c84ec2af8429ccca4866c
Bluboy456 <stuart.jessup@hotmail.co.uk>
2020-12-18 15:00:45 +0000

    Traversing and spraying!

--- move_avoid_zap.py
+++ move_avoid_zap.py
@@ -6,7 +6,7 @@ import tf
 import time
 import geometry_msgs.msg
 from nav_msgs.msg import OccupancyGrid, MapMetaData
-from geometry_msgs.msg import Twist, Point, Quaternion
+from geometry_msgs.msg import Twist, Point, Quaternion, Pose
 from sensor_msgs.msg import LaserScan, Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
@@ -34,9 +34,10 @@ class mapping():
         self.angular_speed = 0.7        #radians per second
         self.mapping_start_time = time.time()
         self.mapping_complete = False   #flag that can be interpreted to find out whether mapping is complete
-        self.mapping_duration = 2      #duration of mapping stage in seconds
+        self.mapping_duration = 120      #duration of mapping stage in seconds
         self.mapping_complete = False
-        self.rate = rospy.Rate(3) 
+        self.rate = rospy.Rate(3)
+
 
 
 
@@ -77,7 +78,7 @@ class mapping():
             angular_vel = 0.0
             if (time_in_secs_mod_ten < 5):
                 angular_vel = (time_in_secs_mod_ten-2)/10.0
-            print 'random angular velocity: ' + str (angular_vel)  #DEBUG
+            #print 'random angular velocity: ' + str (angular_vel)  #DEBUG
 
 
             self.motion.angular.z = angular_vel
@@ -137,6 +138,7 @@ class mapping():
 class weeding:   
 
     def __init__(self):
+        #mapping settings
         self.occupancy_grid = rospy.wait_for_message('/map',OccupancyGrid )
         self.meta_data = rospy.wait_for_message('/map_metadata',MapMetaData ) 
         self.move_base_client = actionlib.SimpleActionClient('/move_base/',MoveBaseAction)
@@ -145,70 +147,95 @@ class weeding:
         self.grid_height = self.meta_data.height * resolution
         self.grid_origin_x = self.meta_data.origin.position.x
         self.grid_origin_y = self.meta_data.origin.position.y
-        '''
+
+        # Camera settings
         self.image_sub = rospy.Subscriber("/thorvald_001/kinect2_camera/hd/image_color_rect",Image,self.camera_callback)
         self.bridge_object = CvBridge()
+        self.camera_interval = 0.2  #time between processing images
+        self.camera_timer_start_time = time.time()
         self.tf_listener = tf.TransformListener()
-        self.calculate_camera_sprayer_transform()
-        '''
+
+        # traversing settings
+        self.traverse_spacing = 0.5     #spacing in metres between traverses
+        self.margin = 3               #distance from boundary of the turning points at end of a traverse (metres)
+
+        # Get distance of sprayer behind camera
+        self.tf_listener.waitForTransform('thorvald_001/kinect2_rgb_optical_frame', 'thorvald_001/sprayer', rospy.Time.now(), rospy.Duration(4.0))
+        self.camera_to_sprayer = self.tf_listener.lookupTransform('thorvald_001/kinect2_rgb_optical_frame', 'thorvald_001/sprayer', rospy.Time())
+        self.camera_sprayer_distance = self.camera_to_sprayer[0][0]
+
+        #spraying settings
+        self.weed_location_queue = []
+        self.pose_sub = rospy.Subscriber("/Pose",Pose,self.update_pose)
+        self.spraying_enabled = False
+        self.weed_location_tolerance = 0.1  #allowable error in sprayer position relative to weed
+        
     
     # moves robot acroos entire map
     # assumes crop planted in y direction so traverses are in y direction
-    def scan_map(self):
-        self.traverse_spacing = 1     #spacing in metres between traverses
-        self.margin = 1               #distance from boundary of the turning points at end of a traverse (metres)
+    def traverse_field(self):
+        self.traverse_up = False      #going downwards
+        self.spraying_enabled = False #don't spray until traversing started
 
-    # move to cell (0,0), ie the'bottom left' corner, allowing for margins
+
+        # move to cell (0,0), ie the'bottom left' corner, allowing for margins
         self.next_x = self.grid_origin_x + self.margin
-        self.next_y = self.grid_origin_y + self.margin
+        self.next_y = self.grid_origin_y + self.margin  
+        '''
+        #DEBUG use this alternative code to move straight to centre so seeing weeds quickly for debug
+        self.next_x = self.grid_origin_x + self.margin + (self.grid_width -2*self.margin)/2    
+        self.next_y = self.grid_origin_y + self.margin + (self.grid_height-2*self.margin)/2  
+        '''
+        #print 'next_y: ' + str(self.next_y) #DEBUG
         self.move_to_goal (self.next_x, self.next_y)
-        self.current_x = self.next_x
+        #print 'reached starting position' #DEBUG
+        self.current_x = self.next_x  
         self.current_y = self.next_y
 
-
+        
         while True:                   
-        # make a traverse   
-            self.next_x = self.current_x 
-            self.next_y = self.current_y + (self.grid_height - self.margin)
+        # make a traverse up 
+            self.traverse_up = True
+            self.spraying_enabled = True
+            self.next_x = self.current_x + (self.grid_width - 2 * self.margin)
+            self.next_y = self.current_y 
             self.move_to_goal (self.next_x, self.next_y)
             self.current_x = self.next_x
             self.current_y = self.next_y
+            self.move_to_goal (self.next_x, self.next_y)
 
         #move across    
-            self.next_x = self.current_x + self.traverse_spacing
-            if self.next_x > (self.grid_width - self.margin):
+            self.spraying_enabled = False     #don't spray while moving to a new row
+            self.next_y = self.current_y + self.traverse_spacing
+            if self.next_y > (self.grid_height - 2 * self.margin):
                 break                                       #gone all the way across
-            self.next_y
-            
-            self.current_y
-            self.move_to_goal (self.next_x, self.next_y
-            )
+            self.next_x = self.current_x
+            self.move_to_goal (self.next_x, self.next_y )
             self.current_x = self.next_x
             self.current_y = self.next_y
+            self.move_to_goal (self.next_x, self.next_y)
 
         # traverse  back 
-            self.next_x = self.current_x 
-            self.next_y
-            
-            self.current_y  - (self.height - self.margin)
-            self.move_to_goal (self.next_x, self.next_y
-            )
+            self.traverse_up = False
+            self.spraying_enabled = True
+            self.next_x = self.current_x - (self.grid_width - 2 * self.margin)
+            self.next_y = self.current_y 
+            self.move_to_goal (self.next_x, self.next_y)
             self.current_x = self.next_x
             self.current_y = self.next_y
+            self.move_to_goal (self.next_x, self.next_y)
 
-        #move across to complete one cycle up and down
-            self.next_x = self.current_x + self.traverse_spacing
-            if self.next_x > (self.grid_width - self.margin):
+        #move across to complete one cycle up and down  
+            self.spraying_enabled = False
+            self.next_y = self.current_y + self.traverse_spacing
+            if self.next_y > (self.grid_height - 2 * self.margin):
                 break                                       #gone all the way across
-            self.next_y
-            
-            self.current_y
-            self.move_to_goal (self.next_x, self.next_y
-            )
+            self.next_x = self.current_x
+            self.move_to_goal (self.next_x, self.next_y )
             self.current_x = self.next_x
             self.current_y = self.next_y
-
-
+            self.move_to_goal (self.next_x, self.next_y)
+    
 
     # The following function is adapted from move_base action_client example at 
     # hotblackrobotics.github.io/en/blog/2018/01/29/action-client-py/
@@ -228,19 +255,21 @@ class weeding:
         else:
             return self.move_base_client.get_result()
 
-'''
-
-    def calculate_camera_sprayer_transform(self):
-    # calculate offset from camera to sprayer so that sprayer can be moved over target
-
-        self.tf_listener.waitForTransform('thorvald_001/kinect2_rgb_optical_frame', 'thorvald_001/sprayer', rospy.Time.now(), rospy.Duration(4.0))
-        self.camera_to_sprayer = self.tf_listener.lookupTransform('thorvald_001/kinect2_rgb_optical_frame', 'thorvald_001/sprayer', rospy.Time())
-        self.x_move = self.camera_to_sprayer[0][0]
-        self.y_move = self.camera_to_sprayer[0][1]
-
+    def update_pose(self,msg):
+            self.pose_x = msg.point.x
+            self.pose_y = msg.point.y
+            # print 'pose x: ' + self.pose_x #DEBUG
+            # print 'pose y: ' + self.pose_y #DEBUG
     def camera_callback(self,data):
-        if self.mapping:
-            return                  #dont need to process camera image or spray when in mapping state
+        '''
+        # control how often camera image is processed
+        if time.time() < self.camera_timer_start_time + self.camera_interval:   
+            return                                   #do nothing if camera interval not up
+        else:
+            self.camera_timer_start_time = time.time()  #else reset timer and carry on to process image
+        '''
+
+        # convert image message to OpenCV format
         try:
             image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
            
@@ -260,7 +289,7 @@ class weeding:
 
         #Create a black & white mask for green
         mask_g = cv2.inRange(hsv, min_green, max_green)
-        cv2.imshow('Green Mask',mask_g)
+        #cv2.imshow('Green Mask',mask_g) #DEBUG
         #colured post-processed image
         res_g = cv2.bitwise_and(image,image, mask= mask_g)
         #cv2.imshow('Green',res_g)
@@ -271,90 +300,71 @@ class weeding:
 
         detector = cv2.SimpleBlobDetector_create()
         keypoints = detector.detect(mask_g)
-        # print keypoints #DEBUG
+        #print keypoints #DEBUG
 	    # Draw detected blobs as red circles.
 	    # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
         res_g_with_blobs = cv2.drawKeypoints(res_g, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        
-        # spray if blob (weed) found TODO move robot to spray in correct place
-        if keypoints:
-            # TODO Cancel current movebase and push goal to stack
-            self.move_over_and_spray()
-
-        # Show blobs
-        #cv2.imshow("Keypoints", res_g_with_blobs)
+       
+        # Show blobs DEBUG
+        cv2.imshow("Keypoints", res_g_with_blobs) #DEBUG
         cv2.waitKey(1)
+       
+       # get current pose of base_link in map
+        now = rospy.Time.now()
+        self.tf_listener.waitForTransform('/map', '/thorvald_001/base_link', now, rospy.Duration(5))
+        (trans, rot) = self.tf_listener.lookupTransform('/map', '/thorvald_001/base_link', now)
+        self.x_pose = trans[0]
+        self.y_pose = trans[1]
 
-    def get_odom(self):
-        # Get the current transform between the odom and base frames
-        try:
-            self.tf_listener.waitForTransform('thorvald_001/odom', 'thorvald_001/base_link', rospy.Time.now(), rospy.Duration(4.0))
-            (trans, rot)  = self.tf_listener.lookupTransform('thorvald_001/odom', 'thorvald_001/base_link', rospy.Time())
-        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
-            rospy.loginfo("TF Exception")
-            return
-        return (Point(*trans))   
+        #clean up weed queue by removing locations that have already been passed, these won't be sprayed
 
-    def move_to_spray(self, x, y):
-        self.pause_movement(1)  #DEBUG
-        position = Point()
-        move_cmd = Twist()
-        goal_distance_x = x 
-        goal_distance_y = y 
-        print ('goal_distance_x:')#DEBUG
-        print  goal_distance_x  #DEBUG
-        print ('goal_distance_y:')#DEBUG
-        print  goal_distance_y  #DEBUG
-        # Set the movement command to forward motion
-        move_cmd.linear.x = self.linear_speed
-        move_cmd.linear.y = self.linear_speed    
-
-        # Get the starting position values     
-        position = self.get_odom()
-                        
-        x_start = position.x
-        y_start = position.y
-
-        distance_x = 0
-        distance_y = 0
-        move_cmd.linear.x = self.linear_speed
-        move_cmd.linear.y = self.linear_speed  
-        #move in x_direction
-        position = self.get_odom()                
-        x_start = position.x
-        while distance_x < goal_distance_x :
-            if self.obstacle_present:   #Abort motion if obstacle present and return False to prevent spray
-                print 'spray move aborted'
-                return False
-            self.cmd_vel_pub.publish(move_cmd)
-            self.rate.sleep()
-            # Get the new position
-            position = self.get_odom()
-            distance_x = math.fabs(position.x - x_start)
-
-            print ('x distance:' + str(distance_x))       #DEBUG
-        
-        #move in y_direction
-        position = self.get_odom()                
-        y_start = position.y
-        while distance_y < goal_distance_y :
-            if self.obstacle_present:   #Abort motion if obstacle present and return False to prevent spray
-                print 'spray move aborted'
-                return False
-            self.cmd_vel_pub.publish(move_cmd)
-            self.rate.sleep()
-            # Get the new position
-            position = self.get_odom()
-            distance_y = math.fabs(position.y - y_start)
+        length = len(self.weed_location_queue)
+        if length > 0:    #only clean up if there are weeds in the queue
+            if self.traverse_up:          #going upwards 
+                length = len(self.weed_location_queue)
+                for i in range(length):
+                    # handle exception caused by queue being modified by another callback thread
+                    try:
+                        if self.weed_location_queue[i] <= self.x_pose + self.weed_location_tolerance:
+                            self.weed_location_queue.pop(i)
+                    except IndexError:
+                        break
+            
+            else:       #going downwards
+                for i in range(len(self.weed_location_queue)):
+                    try:
+                        if self.weed_location_queue[i] >= self.x_pose + self.weed_location_tolerance:
+                            self.weed_location_queue.pop(i)
+                    except IndexError:
+                        break
 
 
-            print ('y distance:' + str(distance_y))  #DEBUG
+        # weed found ...
+        if (keypoints and self.spraying_enabled):
+            #print 'weed found: x pose: ' +str(self.x_pose) #DEBUG
+            #print 'weed found: y pose: ' +str(self.y_pose)#DEBUG
+            #print 'x pose to spray weed = ' + str(self.x_pose + self.camera_sprayer_distance) #DEBUG
+            # add weed location to to-spray list, taking into account direction of motion
+            if self.traverse_up:
+                self.weed_location_queue.append(self.x_pose + self.camera_sprayer_distance)
+            else:
+                self.weed_location_queue.append(self.x_pose - self.camera_sprayer_distance)
 
-        return True
-    def move_over_and_spray(self):
-        #If move to correct spray position was successful, then spray
-        if self.move_to_spray(self.x_move, self.y_move):   
-            self.pause_movement(1)  #DEBUG
+        # check whether current base_link position is such that sprayer is over a weed
+        # looking at the head of the queue of weed locations as that should be the next weed the robot passes over
+        # TODO  Dont spray if y error too large (because avoiding obstacle)
+        if self.weed_location_queue:                                #if queue has weed_locations in it
+            if abs(self.y_pose - self.next_y) < 0.1:         #only spray if cross-track error small
+                #print 'next weed location' + str(self.weed_location_queue[0]) #DEBUG
+                #print  'current location' + str(self.x_pose) #DEBUG
+                #print 'weed error' + str(abs(self.weed_location_queue[0]- self.x_pose)) #DEBUG
+                if abs(self.weed_location_queue[0]- self.x_pose) < self.weed_location_tolerance:    #and sprayer is within 10cm of weed TODO reduce this if possible
+                    self.spray()
+                    self.weed_location_queue.pop(0)          # remove location from the queue now that it has been sprayed
+
+
+    def spray(self):
+        #trigger spray service
             rospy.wait_for_service('/thorvald_001/spray')
             try:
                 spray = rospy.ServiceProxy('/thorvald_001/spray', Empty)
@@ -370,14 +380,14 @@ class weeding:
         self.cmd_vel_pub.publish(move_cmd)
         rospy.sleep(pause_time)
         self.movement_paused = False     #pause is over        
-'''
+
 # Main Code
 if __name__ == '__main__':
     # CREATE MAP
     rospy.init_node('mapping', anonymous=True)
     try:
-        m = mapping()
         '''
+        m = mapping()
         m.map()
         #wait while mapping phase takes place
         while not(m.mapping_complete):
@@ -385,11 +395,8 @@ if __name__ == '__main__':
         '''
         # TRAVERSE FIELD, ID WEEDS AND SPRAY
         #rospy.init_node('weeding', anonymous=True)
-        w = weeding()        
-        move_result = w.move_to_goal(100,100)
-        if move_result:
-            rospy.loginfo("Goal reached")
-
+        w = weeding()     
+        w.traverse_field()   
 
 
     except rospy.ROSInterruptException:
